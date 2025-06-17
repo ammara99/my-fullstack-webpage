3bff5a4c452d017ce0d69bb2a21c0ad6fc131f9d
ammara99 <ammarazamir1993@gmail.com>
2025-06-17 21:01:52 +0200

    Add updated files: .gitignore, .env, package.json, server.js

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files (for your webpage)
+// Serve static files from the 'public' folder (where your index.html is located)
 app.use(express.static('public'));
 
-// Home route
+// Route to serve the index.html file when the root is requested
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
